a63120f96eb97f27f03ff547a7ba1de2554fb6f4
jebt <roelantvanderhilst@gmail.com>
2021-11-30 04:50:35 +0700

    Made somewhat of a framework and template

--- aoc.py
+++ aoc.py
@@ -1,28 +1,8 @@
-YEAR = 2017
-DAY = 1
-
-
-def puzzle_2017_1(puzzle_input):
-    print(find_total(puzzle_input))
-
-    # PART TWO
-    steps_forward = (len(puzzle_input) - 1) // 2
-    print(find_total(puzzle_input, steps=steps_forward))
-
-
-def find_total(puzzle_input, steps=1):
-    total = 0
-    for i, digit in zip(range(len(puzzle_input)-1), puzzle_input):
-        to_compare_index = (i + steps) % (len(puzzle_input) - 1)
-        if digit == puzzle_input[to_compare_index]:
-            total += int(digit)
-    return total
+import year_2017.day_01 as puzzle
 
 
 def main():
-    with open(f"{YEAR}/{DAY}/input.txt") as f:
-        puzzle_input = f.read()
-    puzzle_2017_1(puzzle_input)
+    puzzle.solve()
 
 
 if __name__ == "__main__":
