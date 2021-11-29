3dca70595246a43270ecb81e42ceb5248fe37f4c
jebt <roelantvanderhilst@gmail.com>
2021-11-29 23:15:30 +0700

    Removed redundant modulo to go cirulair on the input data

--- aoc.py
+++ aoc.py
@@ -12,7 +12,6 @@ def puzzle_2017_1(puzzle_input):
 
 def find_total(puzzle_input, steps=1):
     total = 0
-    steps = steps % (len(puzzle_input) - 1)
     for i, digit in zip(range(len(puzzle_input)-1), puzzle_input):
         to_compare_index = (i + steps) % (len(puzzle_input) - 1)
         if digit == puzzle_input[to_compare_index]:
